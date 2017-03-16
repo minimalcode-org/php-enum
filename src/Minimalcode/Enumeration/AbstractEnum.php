e028b5862a17cd6a4eecab7e5ee07844ef4503e5
Fabio Piro <f.piro@miowelfare.it>
2017-03-16 18:23:53 +0100

    Updated enum, psr2, added has* and get*

--- src/Minimalcode/Enumeration/AbstractEnum.php
+++ src/Minimalcode/Enumeration/AbstractEnum.php
@@ -13,232 +13,326 @@ use ReflectionClass;
  */
 abstract class AbstractEnum
 {
-	/**
-	 * The selected enumerator name
-	 *
-	 * @var string
-	 */
-	private $name;
+    /**
+     * The selected enumerator name
+     *
+     * @var string
+     */
+    private $name;
 
-	/**
-	 * The selected enumerator value
-	 *
-	 * @var null|bool|int|float|string
-	 */
-	private $value;
+    /**
+     * The selected enumerator value
+     *
+     * @var null|bool|int|float|string
+     */
+    private $value;
 
-	/**
-	 * The selected enumerator ordinal
-	 *
-	 * @var int
-	 */
-	private $ordinal;
+    /**
+     * The selected enumerator ordinal
+     *
+     * @var int
+     */
+    private $ordinal;
 
-	/**
-	 * Already instantiated enumerators.
-	 *
-	 * @var array ["$class" => ["$name" => $instance, "$name" => $instance, ...], ...]
-	 */
-	private static $instances = [];
+    /**
+     * Already instantiated enumerators.
+     *
+     * @var array ["$class" => ["$name" => $instance, "$name" => $instance, ...], ...]
+     */
+    private static $enumerators = [];
 
-	/**
-	 * Constructor
-	 *
-	 * @param string $name
-	 * @param null|bool|int|float|string $value The value of the enumerator
-	 * @param int $ordinal
-	 */
-	final private function __construct($name, $value, $ordinal)
-	{
-		$this->name = $name;
-		$this->value = $value;
-		$this->ordinal = $ordinal;
-	}
-	/**
-	 * Get an enumerator instance by the given name.
-	 *
-	 * This will be called automatically on calling a method
-	 * with the same name of a defined enumerator.
-	 *
-	 * @param string $method The name of the enumeraotr (called as method)
-	 * @param array $args There should be no arguments
-	 * @return static
-	 * @throws \LogicException On ambiguous constant values
-	 * @throws InvalidArgumentException On an invalid or unknown name
-	 */
-	final public static function __callStatic($method, array $args)
-	{
-		return static::forName($method);
-	}
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param null|bool|int|float|string $value The value of the enumerator
+     * @param int $ordinal
+     */
+    final private function __construct($name, $value, $ordinal)
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->ordinal = $ordinal;
+    }
 
-	/**
-	 * Get an enumerator instance by the given name
-	 *
-	 * @param string $name The name of the enumerator
-	 * @return static
-	 * @throws \LogicException On ambiguous constant values
-	 * @throws InvalidArgumentException On an invalid or unknown name
-	 */
-	final public static function forName($name)
-	{
-		$class = get_called_class();
-		$instances = static::enumeratorsOf($class);
+    /**
+     * Get an enumerator instance by the given name.
+     *
+     * This will be called automatically on calling a method
+     * with the same name of a defined enumerator.
+     *
+     * @param string $method The name of the enumeraotr (called as method)
+     * @param array $args There should be no arguments
+     * @return static
+     * @throws \LogicException On ambiguous constant values
+     * @throws InvalidArgumentException On an invalid or unknown name
+     */
+    final public static function __callStatic($method, array $args)
+    {
+        return static::forName($method);
+    }
 
-		/** @noinspection UnSafeIsSetOverArrayInspection */
-		if(!isset($instances[$name])) {
-			throw new InvalidArgumentException($class . ' has not an enum defined with name \'' . $name . '\'');
-		}
+    /**
+     * Get an enumerator instance by the given name
+     *
+     * @param string $name The name of the enumerator
+     * @return static
+     * @throws \LogicException On ambiguous constant values
+     * @throws InvalidArgumentException On an invalid or unknown name
+     */
+    final public static function forName($name)
+    {
+        $instances = static::getEnumerators();
 
-		return $instances[$name];
-	}
+        if (!static::hasName($name)) {
+            throw new InvalidArgumentException(
+                get_called_class() . ' has not an enum defined with name \'' . $name . '\''
+            );
+        }
 
-	/**
-	 * Get an enumerator instance by the given value
-	 *
-	 * @param null|bool|int|float|string $value
-	 * @return static
-	 * @throws \LogicException On ambiguous constant values
-	 * @throws \InvalidArgumentException On an invalid or unknown value
-	 */
-	final public static function forValue($value)
-	{
-		$class = get_called_class();
+        return $instances[$name];
+    }
 
-		foreach (static::enumeratorsOf($class) as $instance) {
-			if($instance->value === $value) {
-				return $instance;
-			}
-		}
+    /**
+     * Get an enumerator instance by the given value
+     *
+     * @param null|bool|int|float|string $value
+     * @return static
+     * @throws \LogicException On ambiguous constant values
+     * @throws \InvalidArgumentException On an invalid or unknown value
+     */
+    final public static function forValue($value)
+    {
+        foreach (static::getEnumerators() as $instance) {
+            if ($instance->value === $value) {
+                return $instance;
+            }
+        }
 
-		throw new InvalidArgumentException($class . ' has not an enum defined with value \'' . $value . '\'');
-	}
+        throw new InvalidArgumentException(
+            get_called_class() . ' has not an enum defined with value \'' . $value . '\''
+        );
+    }
 
-	/**
-	 * Get an enumerator instance by the given ordinal
-	 *
-	 * @param int $ordinal
-	 * @return static
-	 * @throws \LogicException On ambiguous constant values
-	 * @throws \InvalidArgumentException On an invalid or unknown value
-	 */
-	final public static function forOrdinal($ordinal)
-	{
-		$class = get_called_class();
+    /**
+     * Get an enumerator instance by the given ordinal
+     *
+     * @param int $ordinal
+     * @return static
+     * @throws \LogicException On ambiguous constant values
+     * @throws \InvalidArgumentException On an invalid or unknown value
+     */
+    final public static function forOrdinal($ordinal)
+    {
+        foreach (static::getEnumerators() as $instance) {
+            if ($instance->ordinal === $ordinal) {
+                return $instance;
+            }
+        }
 
-		foreach (static::enumeratorsOf($class) as $instance) {
-			if($instance->ordinal === $ordinal) {
-				return $instance;
-			}
-		}
+        throw new InvalidArgumentException(
+            get_called_class() . ' has not an enum defined with ordinal \'' . $ordinal . '\''
+        );
+    }
 
-		throw new InvalidArgumentException($class . ' has not an enum defined with ordinal \'' . $ordinal . '\'');
-	}
+    /**
+     * @return static[]
+     * @throws \LogicException On ambiguous constant values
+     * @deprecated will be removed, use getEnumerators()
+     */
+    final public static function all()
+    {
+        return static::getEnumerators();
+    }
 
-	/**
-	 * @return static[]
-	 * @throws \LogicException On ambiguous constant values
-	 */
-	final public static function all()
-	{
-		return static::enumeratorsOf(get_called_class());
-	}
+    /**
+     * Get a list of enumerator instances
+     *
+     * @return static[]
+     * @throws \LogicException On ambiguous constant values
+     */
+    final public static function getEnumerators()
+    {
+        return static::enumeratorsOf(get_called_class());
+    }
 
-	/**
-	 * Store and return all available enums by the given class
-	 *
-	 * @param string $class
-	 * @return static[]
-	 * @throws LogicException On ambiguous constant values
-	 */
-	final private static function enumeratorsOf($class)
-	{
-		if (!array_key_exists($class, self::$instances)) {
-			$reflection = new ReflectionClass($class);
-			$constants  = $reflection->getConstants();
+    /**
+     * Get a list of enumerator names
+     *
+     * @return string[]
+     * @throws \LogicException On ambiguous constant values
+     */
+    final public static function getNames()
+    {
+        return array_keys(self::getEnumerators());
+    }
 
-			// values needs to be unique
-			$ambiguous = [];
-			foreach ($constants as $value) {
-				$names = array_keys($constants, $value, true);
-				if (count($names) > 1) {
-					$ambiguous[var_export($value, true)] = $names;
-				}
-			}
+    /**
+     * Get a list of enumerator values
+     *
+     * @return mixed[]
+     * @throws \LogicException On ambiguous constant values
+     */
+    final public static function getValues()
+    {
+        $values = [];
+        foreach (self::getEnumerators() as $enum) {
+            $values[$enum->getOrdinal()] = $enum->getValue();
+        }
 
-			// ambiguous values are not permitted
-			if (count($ambiguous) > 0) {
-				throw new LogicException(
-					'All possible values needs to be unique. The following are ambiguous: '
-					. implode(', ', array_map(function ($names) use ($constants) {
-						return implode('/', $names) . '=' . var_export($constants[$names[0]], true);
-					}, $ambiguous))
-				);
-			}
+        return $values;
+    }
 
-			// cache instances
-			$ordinal = 0;
-			foreach ($constants as $name => $value) {
-				self::$instances[$class][$name] = new $class($name, $value, $ordinal++);
-			}
-		}
+    /**
+     * Get a list of enumerator ordinal numbers
+     *
+     * @return int[]
+     * @throws \LogicException On ambiguous constant values
+     */
+    final public static function getOrdinals()
+    {
+        $count = \count(self::getEnumerators());
+        return $count === 0 ? [] : range(0, $count - 1);
+    }
 
-		return self::$instances[$class];
-	}
+    /**
+     * Checks if an enum with the given name exists
+     *
+     * @param string $name
+     * @return bool
+     * @throws \LogicException
+     */
+    final public static function hasName($name)
+    {
+        return isset(static::getEnumerators()[$name]);
+    }
 
-	/**
-	 * Get the name of the enumerator
-	 *
-	 * @return string
-	 */
-	final public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * Checks if an enum with the given value exists
+     *
+     * @param  null|bool|int|float|string $value
+     * @return bool
+     * @throws \LogicException
+     */
+    final public static function hasValue($value)
+    {
+        foreach (static::getEnumerators() as $instance) {
+            if ($instance->getValue() === $value) {
+                return true;
+            }
+        }
 
-	/**
-	 * Get the value of the enumerator
-	 *
-	 * @return null|bool|int|float|string
-	 */
-	final public function getValue()
-	{
-		return $this->value;
-	}
+        return false;
+    }
 
-	/**
-	 * Get the ordinal of the enumerator
-	 *
-	 * @return int
-	 */
-	final public function getOrdinal()
-	{
-		return $this->ordinal;
-	}
+    /**
+     * Checks if an enum with the given ordinal exists
+     *
+     * @param int $ordinal
+     * @return bool
+     * @throws \LogicException
+     */
+    final public static function hasOrdinal($ordinal)
+    {
+        return \count(static::getEnumerators()) >= $ordinal;
+    }
 
-	/**
-	 * Get the name of the enumerator
-	 *
-	 * @return string
-	 * @see getName()
-	 */
-	public function __toString()
-	{
-		return $this->name;
-	}
+    /**
+     * Store and return all available enums by the given class
+     *
+     * @param string $class
+     * @return static[]
+     * @throws LogicException On ambiguous constant values
+     */
+    final private static function enumeratorsOf($class)
+    {
+        if (!array_key_exists($class, self::$enumerators)) {
+            $reflection = new ReflectionClass($class);
+            $constants = $reflection->getConstants();
 
-	/**
-	 * @throws LogicException Enums are not cloneable because instances are implemented as singletons
-	 */
-	final private function __clone()
-	{
-		throw new LogicException('Enums are not cloneable');
-	}
+            // values needs to be unique
+            $ambiguous = [];
+            foreach ($constants as $value) {
+                $names = array_keys($constants, $value, true);
+                if (count($names) > 1) {
+                    $ambiguous[var_export($value, true)] = $names;
+                }
+            }
 
-	/**
-	 * @throws LogicException Enums are not serializable because instances are implemented as singletons
-	 */
-	final public function __wakeup()
-	{
-		throw new LogicException('Enums are not serializable');
-	}
+            // ambiguous values are not permitted
+            if (count($ambiguous) > 0) {
+                throw new LogicException(
+                    'All possible values needs to be unique. The following are ambiguous: '
+                    . implode(', ', array_map(function ($names) use ($constants) {
+                        return implode('/', $names) . '=' . var_export($constants[$names[0]], true);
+                    }, $ambiguous))
+                );
+            }
+
+            // cache instances
+            $ordinal = 0;
+            foreach ($constants as $name => $value) {
+                self::$enumerators[$class][$name] = new $class($name, $value, $ordinal++);
+            }
+        }
+
+        return self::$enumerators[$class];
+    }
+
+    /**
+     * Get the name of the enumerator
+     *
+     * @return string
+     */
+    final public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of the enumerator
+     *
+     * @return null|bool|int|float|string
+     */
+    final public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Get the ordinal of the enumerator
+     *
+     * @return int
+     */
+    final public function getOrdinal()
+    {
+        return $this->ordinal;
+    }
+
+    /**
+     * Get the name of the enumerator
+     *
+     * @return string
+     * @see getName()
+     */
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @throws LogicException Enums are not cloneable because instances are implemented as singletons
+     */
+    final private function __clone()
+    {
+        throw new LogicException('Enums are not cloneable');
+    }
+
+    /**
+     * @throws LogicException Enums are not serializable because instances are implemented as singletons
+     */
+    final public function __wakeup()
+    {
+        throw new LogicException('Enums are not serializable');
+    }
 }
